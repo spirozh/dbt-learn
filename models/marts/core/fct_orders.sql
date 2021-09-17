666f18ec579a683bb73fd5326a4694ac1eb77607
jason songhurst <jason.songhurst@nocdhelp.com>
2021-09-17 13:49:42 +0000

    copied exemplar

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -1,18 +1,30 @@
-orders as (
-
-    select * from {{ ref('stg_orders') }}
-
+with orders as  (
+    select * from {{ ref('stg_orders' )}}
 ),
 
 payments as (
-
     select * from {{ ref('stg_payments') }}
 ),
 
-final as (
-    select orders.order_id, orders.payment_id, payments.amount
-    from orders join payments using (order_id)
+order_payments as (
+    select
+        order_id,
+        sum(case when status = 'success' then amount end) as amount
 
+    from payments
+    group by 1
 ),
+
+final as (
+
+    select
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        coalesce(order_payments.amount, 0) as amount
+
+    from orders
+    left join order_payments using (order_id)
+)
 
 select * from final
